db797516cb39e0aca602b78fbd9e0a990efd10a9
volldammer <oskarwhite@gmail.com>
2016-11-18 17:44:25 +0100

    Suma calculada

---
+++ calcular.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * codigo calcular suma, media e impares
+ */
+
+$numeros[]=$_POST["n1"];
+$numeros[]=$_POST["n2"];
+$numeros[]=$_POST["n3"];
+$numeros[]=$_POST["n4"];
+$numeros[]=$_POST["n5"];
+$numeros[]=$_POST["n6"];
+
+ function media($a) // función para calcular el valor medio de un vector
+    {
+        $res = 0;
+        for($i=0;$i<count($a);$i++)
+            $res += $a[$i];
+        $res /= count($a);
+        return $res;
+    }
+    
+    $media_nums = media($numeros);
+   echo"La media de los numeros es: $media_nums<br>";
+    
+   
--- index.php
+++ index.php
@@ -8,6 +8,16 @@ Formulario que pida 6 numeros y calcule::suma//media//¿cuantos son pares?
         <title></title>
     </head>
     <body>
+          <form action="calcular.php" method="POST">
+            <p>Introduce 6 numeros:</p>
+            <p><input type="number" name="n1" ></p>
+                <p><input type="number" name="n2" ></p>
+                    <p><input type="number" name="n3" ></p>
+                        <p><input type="number" name="n4" ></p>
+                            <p><input type="number" name="n5" ></p>
+                                <p><input type="number" name="n6" ></p>
+                                
+                                <p><input type="submit" name="enviar" value="enviar datos"></p>       
       
     </body>
 </html>
