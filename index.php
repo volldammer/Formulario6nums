f57a55a1756fb944c978a1104a7937273cd7d754
volldammer <oskarwhite@gmail.com>
2016-11-18 17:21:34 +0100

    Git inicializado

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--
+Formulario que pida 6 numeros y calcule::suma//media//¿cuantos son pares?
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+      
+    </body>
+</html>
